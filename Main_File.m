6ef8ab3d76b564e1b8e41dd3e14b7f87087fe619
Mohammad Badri Ahmadi <mhmmdbdrhmd@gmail.com>
2017-08-21 10:33:18 -0500

    Add Loading Files from the data directory for LFP data, take note that all the units have the same LFP, so reading from one unit is enough

--- Main_File.m
+++ Main_File.m
@@ -1,1 +1,12 @@
-clc
+clc; clear all; close all;
+
+ParentFileSection1="D:/DATA\Monkey/The_Other_Monkey_From_Junmo/Extracted_Spikes_LFPs_SFCs_STAs/Monkey_PG/";
+ParentFileSection2="/Spikes_LFPs_PAC_SFC_STA/M1_PG_Manual_Extracted_Spikes_LFP_unit_num_1.mat";
+
+different_sessions={"20170315-1", "20170315-2","20170316-1","20170316-2","20170316-3","20170317-1","20170317-2","20170317-3","20170328" };
+
+for i = 1 : size (different_sessions,2)
+   load (strcat(ParentFileSection1, different_sessions{i},ParentFileSection2));
+   
+    
+end
