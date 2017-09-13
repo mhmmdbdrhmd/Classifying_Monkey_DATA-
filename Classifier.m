8c7f13f4ddf0f6d5258a1b906cfea9fd7c7c3f73
Mohammad Badri Ahmadi <mhmmdbdrhmd@gmail.com>
2017-09-12 19:08:29 -0500

    1- it is using parfor, 2- now it is using start point 3- start point have been added to saving and loading file 4-two for loops have been added in order to change W(end point) and start point

--- Classifier.m
+++ Classifier.m
@@ -1,54 +1,69 @@
 clear all; clc; close all; warning off;
 
-Window_Size=500; %Sample
-diff = 499;
-f=1:0.5:200;
+File=1;
 
-load (['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD' ,'\W',int2str(Window_Size), '_OL', int2str(diff),'_Postreward_Binary_Feature.mat']);
-load (['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD' ,'\W',int2str(Window_Size), '_OL', int2str(diff),'_Postreward_Binary_Output.mat']);
-load (['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD' ,'\W',int2str(Window_Size), '_OL', int2str(diff),'_Postreward_Binary_st.mat']);
+for SP= 1 : 10 : 491
+    for Window = 501 - SP
 
-%Feature=frequency_extracter_from_PSD(Feature, f, 'A');
+        Window_Size=Window; %Sample
+        f=1:0.5:200; %Frequencies for the PSD
+        starting_point=SP;
+        diff = 1000;
 
-data= [Feature,Output];
+        load (['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD' ,'\W',int2str(Window_Size), '_OL', int2str(diff),'_S',int2str(starting_point),'_Postreward_Binary_Feature.mat']);
+        load (['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD' ,'\W',int2str(Window_Size), '_OL', int2str(diff),'_S',int2str(starting_point),'_Postreward_Binary_Output.mat']);
+        load (['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD' ,'\W',int2str(Window_Size), '_OL', int2str(diff),'_S',int2str(starting_point),'_Postreward_Binary_st.mat']);
 
-clear Feature Output
+        %Feature=frequency_extracter_from_PSD(Feature, f, 'A');
 
-totaltrial=max(st(:,2)); trial=1:totaltrial; 
-partn = cvpartition(trial,'k',6);
+        data= [Feature,Output];
 
-for iipart = 1:partn.NumTestSets
-    traintrialIndx = find(partn.training(iipart)==1);
-    trainIndx=ismember(st(:,2),traintrialIndx);
-    TESTtrialIndx = find(partn.test(iipart)==1);
-    TESTIndx=ismember(st(:,2),TESTtrialIndx);
-    datatr=data(trainIndx,:);
-    datatr=datatr(randperm(size(datatr,1)),:);
-    datate=data(TESTIndx,:);
-    datate=datate(randperm(size(datate,1)),:);
-    [cl,computational_time]=binary_classifier_all(datatr);
-    
-    
-    
-    accuracyte(:,iipart)=accuracy_binary_classifier_all(datate,cl);
-    disp([int2str(iipart), ' th part finished']); pause(0.1)
+        clear Feature Output
+
+        totaltrial=max(st(:,2)); trial=1:totaltrial; 
+        partn = cvpartition(trial,'k',12);
+        
+        fprintf(['STARTING ', int2str(File),' th FILE\n'])
+        parfor iipart = 1:partn.NumTestSets
+            traintrialIndx = find(partn.training(iipart)==1);
+            trainIndx=ismember(st(:,2),traintrialIndx);
+            TESTtrialIndx = find(partn.test(iipart)==1);
+            TESTIndx=ismember(st(:,2),TESTtrialIndx);
+            datatr=data(trainIndx,:);
+            datatr=datatr(randperm(size(datatr,1)),:);
+            datate=data(TESTIndx,:);
+            datate=datate(randperm(size(datate,1)),:);
+            [cl,computational_time]=binary_classifier_all(datatr);
+            accuracyte(File,:,iipart)=accuracy_binary_classifier_all(datate,cl);
+            disp([int2str(iipart), ' th part finished']); pause(0.1)
+        end
+
+        %TITLE of each 23 classifier in cl, accuracytr, and accuracyte.
+        tit=title_binary_classifier_all();
+
+        %Temporary Accuracy
+        TemporarySize=size(accuracyte);
+        TA=reshape(accuracyte(File,:,:),[TemporarySize(2),TemporarySize(3)]);
+        
+        % Finding Highest Chance 
+        chance1=st(:,3)==1; chance2=st(:,3)==2; hchance=max([sum(chance1),sum(chance2)])/size(st,1)*100;
+
+        %Finding out the highes accuracy in test:
+        [a1,a2]=max(TA(:)); [z1,z2]=ind2sub(size(TA),a2);
+        fprintf(['Highest accuracy on test set is ', num2str(a1*100) ,' percent.\n'...
+            'And it is for ', tit{z1}, ' classifier, on the ', int2str(z2), 'th partition.\n'...
+            'The chance is = ', num2str(hchance),'\n\n']);
+
+        %Finding out the highes cross validated accuracy in test
+        meanacc=mean(TA,2);
+        a3=max(meanacc); a4=find(meanacc==a3);
+        fprintf(['Highest real accuracy on test is ', num2str(a3*100),' percent.\n'...
+            'And it is for ',tit{a4},' classifier.\n'...
+            'The chance is = ', num2str(hchance),'\n\n']);
+        
+        fprintf('-------------------------------------------------------- \n\n');
+        
+        
+        File=File+1;
+    end
 end
-
-%TITLE of each 23 classifier in cl, accuracytr, and accuracyte.
-tit=title_binary_classifier_all();
-
-% Finding Highest Chance 
-chance1=st(:,3)==1; chance2=st(:,3)==2; hchance=max([sum(chance1),sum(chance2)])/size(st,1)*100;
-
-%Finding out the highes accuracy in test:
-[a1,a2]=max(accuracyte(:)); [z1,z2]=ind2sub(size(accuracyte),a2);
-fprintf(['Highest accuracy on test set is ', num2str(a1*100) ,' percent.\n'...
-    'And it is for ', tit{z1}, ' classifier, on the ', int2str(z2), 'th partition.\n'...
-    'The chance is = ', num2str(hchance),'\n\n']);
-
-%Finding out the highes cross validated accuracy in test
-meanacc=mean(accuracyte,2);
-a3=max(meanacc); a4=find(meanacc==a3);
-fprintf(['Highest real accuracy on test is ', num2str(a3*100),' percent.\n'...
-    'And it is for',tit{a4},' classifier.\n'...
-    'The chance is = ', num2str(hchance),'\n']);
--- Main_File.m
+++ Main_File.m
@@ -6,55 +6,64 @@ ParentFileSection1="D:/DATA\Monkey/The_Other_Monkey_From_Junmo/Extracted_Spikes_
 ParentFileSection2="/Spikes_LFPs_PAC_SFC_STA/M1_PG_Manual_Extracted_Spikes_LFP_unit_num_1.mat";
 different_sessions={"20170315-1", "20170315-2","20170316-1","20170316-2","20170316-3","20170317-1","20170317-2","20170317-3","20170328" };
 
-%HP for PSD 
-Window_Size=500; %Sample
-nw=3.5; %Should check later
-f=1:0.5:200; %Frequencies for the PSD
-fs=1000; %Sampling rate
+for SP = 1 : 10 : 491
+    for Window = 501-SP
 
-Counter = 1;
-TS=[];%Temproray Signal
-Feature=[];
-Output=[];
-diff = 499;
-ttrial=1;
+        
+        Window_Size=Window; %Sample
+        f=1:0.5:200; %Frequencies for the PSD
+        starting_point=SP;
+        diff = 1000;
+        
+        %HP for PSD 
+        fs=1000; %Sampling rate
+        nw=3.5; %Should check later
 
-for i = 1 : size (different_sessions,2)  % i : different sessions
-   load (strcat(ParentFileSection1, different_sessions{i},ParentFileSection2));
-   
-   %R3P0 = 1
-   for j = 1 : cnt_R3P0_trials  %j : Different Trials
-       TS=[];
-       TS=TrialLFP_R3P0_Postreward(j).LFP;
-       for k = 1 : diff : size(TS,2) - (Window_Size -1) %k : Different time points
-           Feature(Counter,:)=pmtm(TS(k : k + (Window_Size -1)), nw, f, fs);
-           Output(Counter,1)=1; st(Counter,2)=ttrial; st(Counter,1)=i; st(Counter,3)=Output(Counter,1);
-           Counter=Counter+1;
-       end
-       disp(['R3P0 for the ', int2str(i),' th session and ', int2str(j), 'th trial is done.']); pause(0.1)
-       ttrial=ttrial+1;
-   end
-   
-   
-   %R0P3 = 2
-   for j = 1 : cnt_R0P3_trials  %j : Different Trials
-       TS=[];
-       TS=TrialLFP_R0P3_Postreward(j).LFP;
-       for k = 1 : diff : size(TS,2) - (Window_Size -1) %k : Different time points
-           Feature(Counter,:)=pmtm(TS(k : k + (Window_Size -1)), nw, f, fs);
-           Output(Counter,1)=2; st(Counter,2)=ttrial; st(Counter,1)=i; st(Counter,3)=Output(Counter,1);
-           Counter=Counter+1;
-       end
-       disp(['R0P3 for the ', int2str(i),' th session and ', int2str(j), 'th trial is done.']); pause(0.1)
-       ttrial=ttrial+1;
-   end
-   
+        Counter = 1;
+        TS=[];%Temproray Signal
+        Feature=[];
+        Output=[];
+        ttrial=1;
+
+        for i = 1 : size (different_sessions,2)  % i : different sessions
+           load (strcat(ParentFileSection1, different_sessions{i},ParentFileSection2));
+
+           %R3P0 = 1
+           for j = 1 : cnt_R3P0_trials  %j : Different Trials
+               TS=[];
+               TS=TrialLFP_R3P0_Postreward(j).LFP;
+               for k = starting_point : diff : size(TS,2) - (Window_Size -1) %k : Different time points
+                   Feature(Counter,:)=pmtm(TS(k : k + (Window_Size -1)), nw, f, fs);
+                   Output(Counter,1)=1; st(Counter,2)=ttrial; st(Counter,1)=i; st(Counter,3)=Output(Counter,1);
+                   Counter=Counter+1;
+               end
+               disp(['R3P0 for the ', int2str(i),' th session and ', int2str(j), 'th trial is done.']); pause(0.1)
+               ttrial=ttrial+1;
+           end
+
+
+           %R0P3 = 2
+           for j = 1 : cnt_R0P3_trials  %j : Different Trials
+               TS=[];
+               TS=TrialLFP_R0P3_Postreward(j).LFP;
+               for k = starting_point : diff : size(TS,2) - (Window_Size -1) %k : Different time points
+                   Feature(Counter,:)=pmtm(TS(k : k + (Window_Size -1)), nw, f, fs);
+                   Output(Counter,1)=2; st(Counter,2)=ttrial; st(Counter,1)=i; st(Counter,3)=Output(Counter,1);
+                   Counter=Counter+1;
+               end
+               disp(['R0P3 for the ', int2str(i),' th session and ', int2str(j), 'th trial is done.']); pause(0.1)
+               ttrial=ttrial+1;
+           end
+
+        end
+
+
+
+
+
+        save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD' ,'\W',int2str(Window_Size), '_OL', int2str(diff),'_S',int2str(starting_point),'_Postreward_Binary_Feature.mat'],'Feature');
+        save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD','\W',int2str(Window_Size), '_OL', int2str(diff),'_S',int2str(starting_point),'_Postreward_Binary_Output.mat'],'Output');
+        save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD','\W',int2str(Window_Size), '_OL', int2str(diff),'_S',int2str(starting_point),'_Postreward_Binary_st.mat'],'st');
+
+    end
 end
-   
-   
-   
-   
-
-save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD' ,'\W',int2str(Window_Size), '_OL', int2str(diff),'_Postreward_Binary_Feature.mat'],'Feature');
-save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD','\W',int2str(Window_Size), '_OL', int2str(diff),'_Postreward_Binary_Output.mat'],'Output');
-save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD','\W',int2str(Window_Size), '_OL', int2str(diff),'_Postreward_Binary_st.mat'],'st');
