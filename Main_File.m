9571e2e2a61fc9321358c773d9e8a95f2eb6493b
Mohammad Badri Ahmadi <mhmmdbdrhmd@gmail.com>
2017-09-15 15:00:01 -0500

    start point added, parfor is using, 12 crossvalidation

--- Main_File.m
+++ Main_File.m
@@ -6,18 +6,21 @@ ParentFileSection1="D:/DATA\Monkey/The_Other_Monkey_From_Junmo/Extracted_Spikes_
 ParentFileSection2="/Spikes_LFPs_PAC_SFC_STA/M1_PG_Manual_Extracted_Spikes_LFP_unit_num_1.mat";
 different_sessions={"20170315-1", "20170315-2","20170316-1","20170316-2","20170316-3","20170317-1","20170317-2","20170317-3","20170328" };
 
-%HP for PSD 
 Window_Size=1000; %Sample
-nw=3.5; %Should check later
 f=1:0.5:200; %Frequencies for the PSD
+starting_point=1;
+diff = 1000;
+
+%HP for PSD 
+nw=3.5; %Should check later
 fs=1000; %Sampling rate
 
 Counter = 1;
 TS=[];%Temproray Signal
 Feature=[];
 Output=[];
-diff = 50;
 ttrial=1;
+
 
 for i = 1 : size (different_sessions,2)  % i : different sessions
    load (strcat(ParentFileSection1, different_sessions{i},ParentFileSection2));
@@ -26,7 +29,7 @@ for i = 1 : size (different_sessions,2)  % i : different sessions
    for j = 1 : cnt_R3P0_trials  %j : Different Trials
        TS=[];
        TS=TrialLFP_R3P0_whole(j).LFP(end-1000:end);
-       for k = 1 : diff : size(TS,2) - (Window_Size -1) %k : Different time points
+       for k = starting_point : diff : size(TS,2) - (Window_Size -1) %k : Different time points
            Feature(Counter,:)=pmtm(TS(k : k + (Window_Size -1)), nw, f, fs);
            Output(Counter,1)=1; st(Counter,2)=ttrial; st(Counter,1)=i; st(Counter,3)=Output(Counter,1);
            Counter=Counter+1;
@@ -40,7 +43,7 @@ for i = 1 : size (different_sessions,2)  % i : different sessions
    for j = 1 : cnt_R0P3_trials  %j : Different Trials
        TS=[];
        TS=TrialLFP_R0P3_whole(j).LFP(end-1000:end);
-       for k = 1 : diff : size(TS,2) - (Window_Size -1) %k : Different time points
+       for k = starting_point : diff : size(TS,2) - (Window_Size -1) %k : Different time points
            Feature(Counter,:)=pmtm(TS(k : k + (Window_Size -1)), nw, f, fs);
            Output(Counter,1)=2; st(Counter,2)=ttrial; st(Counter,1)=i; st(Counter,3)=Output(Counter,1);
            Counter=Counter+1;
@@ -52,6 +55,6 @@ for i = 1 : size (different_sessions,2)  % i : different sessions
    
 end
 
-save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD' ,'\W',int2str(Window_Size), '_OL', int2str(diff),'_prepostreward_Binary_Feature.mat'],'Feature');
-save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD','\W',int2str(Window_Size), '_OL', int2str(diff),'_prepostreward_Binary_Output.mat'],'Output');
-save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD','\W',int2str(Window_Size), '_OL', int2str(diff),'_prepostreward_Binary_st.mat'],'st');
+save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD' ,'\W',int2str(Window_Size), '_OL', int2str(diff),'_S',int2str(starting_point),'_prepostreward_Binary_Feature.mat'],'Feature');
+save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD','\W',int2str(Window_Size), '_OL', int2str(diff),'_S',int2str(starting_point),'_prepostreward_Binary_Output.mat'],'Output');
+save(['D:\DATA\Monkey\The_Other_Monkey_From_Junmo\Extracted_PSD','\W',int2str(Window_Size), '_OL', int2str(diff),'_S',int2str(starting_point),'_prepostreward_Binary_st.mat'],'st');
